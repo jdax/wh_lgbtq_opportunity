008a3af8e60b8953757b030ee7e053072d0e83e9
Nikki Murray <nikki.liz.murray@gmail.com>
2015-11-30 23:08:31 -0500

    getting mentroship controller set up

--- app/controllers/mentorships_controller.rb
+++ app/controllers/mentorships_controller.rb
@@ -1,5 +1,21 @@
 class MentorshipsController < ApplicationController
+  helper_method :sort_column, :sort_direction
   make_resourceful do
     actions :all
+  end
+
+
+
+  def index
+    @mentorships = Mentorship.order(sort_column + ' ' + sort_direction)
+  end
+
+  private
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
+  def sort_column
+    Mentorship.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
   end
 end
