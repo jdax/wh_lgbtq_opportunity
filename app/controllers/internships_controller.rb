c1e24c39bd845cd7140370ca7f5bacdffb594739
Nikki Murray <nikki.liz.murray@gmail.com>
2015-11-30 23:07:55 -0500

    getting internship controller set up

--- app/controllers/internships_controller.rb
+++ app/controllers/internships_controller.rb
@@ -1,5 +1,21 @@
 class InternshipsController < ApplicationController
+  helper_method :sort_column, :sort_direction
   make_resourceful do
     actions :all
+  end
+
+
+
+  def index
+    @internships = Internship.order(sort_column + ' ' + sort_direction)
+  end
+
+  private
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
+  def sort_column
+    Internship.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
   end
 end
