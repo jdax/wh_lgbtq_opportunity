e9f57965bcee026a70b5c9d6297a831a973b5ac6
Nikki Murray <nikki.liz.murray@gmail.com>
2015-11-30 23:07:09 -0500

    getting events controller set up

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,21 @@
 class EventsController < ApplicationController
+  helper_method :sort_column, :sort_direction
   make_resourceful do
     actions :all
+  end
+
+
+
+  def index
+    @events = Event.order(sort_column + ' ' + sort_direction)
+  end
+
+  private
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
+  def sort_column
+    Event.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
   end
 end
