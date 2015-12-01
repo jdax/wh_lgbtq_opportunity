6dd2956ecb75f2d59f264c8842caa44c8ef4f076
Nikki Murray <nikki.liz.murray@gmail.com>
2015-11-30 23:09:17 -0500

    getting scholarship controller set up

--- app/controllers/scholarships_controller.rb
+++ app/controllers/scholarships_controller.rb
@@ -1,5 +1,21 @@
 class ScholarshipsController < ApplicationController
+  helper_method :sort_column, :sort_direction
   make_resourceful do
     actions :all
+  end
+
+
+
+  def index
+    @scholarships = Scholarship.order(sort_column + ' ' + sort_direction)
+  end
+
+  private
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
+  def sort_column
+    Scholarship.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
   end
 end
